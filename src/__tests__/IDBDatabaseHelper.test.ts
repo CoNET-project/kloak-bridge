63652855ac7eb30a5ec1f4bd84bbe0d478049330
aybeeseedee <80135066+aybeeseedee@users.noreply.github.com>
2021-03-19 11:36:47 -0700

    Fixed ESLint configuration, fix IDBDatabaseHelper tests.

--- src/__tests__/IDBDatabaseHelper.test.ts
+++ src/__tests__/IDBDatabaseHelper.test.ts
@@ -1,50 +1,31 @@
-require("fake-indexeddb/auto");
 import IDBDatabaseHelper from '../IDBDatabaseHelper';
 
+require('fake-indexeddb/auto');
 
-
-describe("IDBDatabaseHelper Class", () => {
-    const uuid = "5f586bb1-dbee-43c0-861e-5c0dd6911b5c";
+describe('IDBDatabaseHelper Class', () => {
+    const fileUuid = '5f586bb1-dbee-43c0-861e-5c0dd6911b5c';
     const testData = {
-        filename: "myfile.txt",
+        filename: 'myfile.txt',
         size: 124,
         date: new Date()
-    }
+    };
 
-    test("Should successfully return an object store", (done) => {
+    test('Should successfully return an object store', async () => {
         const idb = new IDBDatabaseHelper();
-        const initCallback = jest.fn((err: any, objectStore: IDBObjectStore | null) => {
-            expect(typeof objectStore?.name).toBe('string');
-            done();
-        });
+        const objectStore = await idb.getObjectStore();
+        expect(objectStore).toBeTruthy();
+    });
 
-        idb.init(initCallback);
-    })
-
-    test('Should save data into IndexedDB', (done) => {
+    test('Should save data into IndexedDB', async () => {
         const idb = new IDBDatabaseHelper();
-        const saveCallback = jest.fn((result: string) => {
-            expect(result).toBe(uuid);
-            done();
-        })
-        idb.save(uuid, testData).then(saveCallback).catch(err => {
-            expect(err).toBe(null);
-        })
-    })
+        const uuid = await idb.save(fileUuid, testData);
+        expect(uuid).toBe(fileUuid);
+    });
 
-    test('Should delete data from IndexedDB', (done) => {
+    test('Should delete data from IndexedDB', async () => {
         const idb = new IDBDatabaseHelper();
-
-        const deleteCallback = jest.fn((result: string) => {
-            expect(result).toBe(uuid);
-            done();
-        })
-
-        idb.save(uuid, testData).then(uuid => {
-            idb.delete(uuid).then(deleteCallback).catch(err => expect(err).toBe(null));
-        })
-    })
-})
-
-
-
+        await idb.save(fileUuid, testData);
+        const deleteUuid = await idb.delete(fileUuid);
+        expect(deleteUuid).toBe(fileUuid);
+    });
+});
