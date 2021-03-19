70656b503c44f8e40d8018a6b018a7fb7af0cb99
aybeeseedee <80135066+aybeeseedee@users.noreply.github.com>
2021-03-18 21:55:19 -0700

    Fix TypeScript-ESLint configurations

--- src/StorageHelper.ts
+++ src/StorageHelper.ts
@@ -1,27 +1,25 @@
 import Encrypt from './Encrypt';
 import IDBDatabaseHelper from './IDBDatabaseHelper';
+// eslint-disable-next-line import/no-unresolved
 import { PGPGenerateOptions, StringPGPKeys } from './define';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 class StorageHelper {
-    private encrypt = new Encrypt();
+    private encrypt: Encrypt = new Encrypt();
     private IDBHelper = new IDBDatabaseHelper();
-    constructor() {
-
-    }
-
-    public newKey = (options: PGPGenerateOptions, unlock?: boolean): Promise<StringPGPKeys | boolean> => {
-        return new Promise(async(resolve, reject) => {
+    public newKey = (options: PGPGenerateOptions, unlock?: boolean): Promise<StringPGPKeys | boolean> => (
+        new Promise(async (resolve, reject) => {
             const stringPGPKeys: StringPGPKeys = await this.encrypt.generateKey(options);
             if (unlock) {
                 const isUnlocked = await this.encrypt.checkPassword(stringPGPKeys, options.passphrase);
-                stringPGPKeys['unlocked'] = isUnlocked;
-                isUnlocked ? resolve(stringPGPKeys) : reject(new Error('Unable to unlock your new OpenPGP key.'))
+                stringPGPKeys.unlocked = isUnlocked;
+                if (isUnlocked) {
+                    return resolve(stringPGPKeys);
+                }
+                return reject(new Error('Unable to unlock your new OpenPGP key.'));
             }
             return resolve(stringPGPKeys);
         })
-    }
-
-
-
+    )
 
 }
