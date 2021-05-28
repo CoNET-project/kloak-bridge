e6cc0198818b5f49a31b8c610f05ad29ba7d90c1
aybeeseedee <80135066+aybeeseedee@users.noreply.github.com>
2021-05-28 14:17:56 -0700

    Fix init

--- src/KloakBridge.ts
+++ src/KloakBridge.ts
@@ -59,18 +59,20 @@ class KloakBridge {
     private retryAttempts = 0;
     private MAX_RETRY_ATTEMPTS = 3;
 
-    constructor(private onInitialized: () => void, private networkListener: NetworkStatusListeners, skipNetwork = false, private localServerPath?: string) {
+    constructor(private onInitialized: () => void, private networkListener: NetworkStatusListeners, private skipNetwork = false, private localServerPath?: string) {
         this.skipNetwork = skipNetwork;
         this.init();
     }
 
     private init = async () => {
-        if ((typeof process !== 'undefined') && (process.release) && (process.release.name === 'node')) {
-            this.getNetworkInformation(this.localServerPath || 'http://localhost:3000/');
-        } else {
-            this.getNetworkInformation(this.localServerPath || window.location.href);
+        if (!this.skipNetwork) {
+            if ((typeof process !== 'undefined') && (process.release) && (process.release.name === 'node')) {
+                this.getNetworkInformation(this.localServerPath || 'http://localhost:3000/');
+            } else {
+                this.getNetworkInformation(this.localServerPath || window.location.href);
+            }
+            this.testNetworkConnection(KloakBridge.seguroConnection.host, KloakBridge.seguroConnection.port);
         }
-        this.testNetworkConnection(KloakBridge.seguroConnection.host, KloakBridge.seguroConnection.port);
     }
 
     public testNetworkConnection = (host: string, port: string | number): Promise<[status: 'IMAP_AVAILABLE' | 'IMAP_UNAVAILABLE']> => (
