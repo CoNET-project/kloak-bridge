cde5cb77197a065f2cdc7e14181b526a599af57c
aybeeseedee <80135066+aybeeseedee@users.noreply.github.com>
2021-06-10 16:08:40 -0700

    Update appState

--- src/appState.ts
+++ src/appState.ts
@@ -1,57 +1,80 @@
-const deleteAllProperties = (object: any) => (
+const deleteAllProperties = (
+    object: any
+) => (
     Object
         .entries(object)
         .forEach(([key, value]) => {
             if (typeof value === 'object') {
                 deleteAllProperties(value);
             }
+            // eslint-disable-next-line no-param-reassign
             delete object[key];
         })
 );
 
+// eslint-disable-next-line import/prefer-default-export
 export const getAppState = <AppStateType>(
     initialState: AppStateType
 ) => {
-    const appState: AppStateType = initialState;
+    const appState = initialState;
 
     const status = {
-        isModified: false
+        isModified: false,
+        isWiped: false
+    };
+
+    const throwIsWipedError = () => {
+        if (status.isWiped) {
+            throw new Error('the state is wiped, create a new instance to proceed');
+        }
     };
 
     const toJSON = (
         pretty?: boolean
-    ) => (
-        JSON.stringify(
+    ) => {
+        throwIsWipedError();
+
+        return JSON.stringify(
             appState,
             null,
             pretty ? 4 : undefined
-        )
-    );
+        );
+    };
 
     const get = <ValueType>(
         getFn: (state: AppStateType) => ValueType
-    ): ValueType => (
-        getFn(appState)
-    );
+    ): ValueType => {
+        throwIsWipedError();
+
+        return getFn(appState);
+    };
 
     const set = (
         setFn: (state: AppStateType) => void
     ) => {
-        setFn(appState);
+        throwIsWipedError();
 
+        setFn(appState);
         status.isModified = true;
     };
 
-    const getIsModified = () => (
-        status.isModified
-    );
+    const getIsModified = () => {
+        throwIsWipedError();
+
+        return status.isModified;
+    };
 
     const clearIsModified = () => {
+        throwIsWipedError();
+
         status.isModified = false;
     };
 
     const wipeState = () => {
+        throwIsWipedError();
+
         deleteAllProperties(appState);
+        status.isWiped = true;
     };
 
     return {
